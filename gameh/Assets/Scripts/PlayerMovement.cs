dc22784e49a6624728a83333ba7bd80a89e36c44
Danyul <scratchmaker500@gmail.com>
2021-04-29 20:17:58 -0700

    movement

---
+++ gameh/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 4f;
+    public float gravityMultiplier = 3f;
+
+    public Rigidbody2D rb; 
+    
+    void Update()
+    {
+        var move = Input.GetAxisRaw("Horizontal");
+         transform.position = transform.position + new Vector3(move * moveSpeed * Time.deltaTime, 0, 0);
+
+        if (Input.GetButton("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (gravityMultiplier - 1) * Time.deltaTime;
+        }
+
+    }
+}
