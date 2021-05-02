44fe0e7083fb694969864391509dfbfe959b098e
Danyul <scratchmaker500@gmail.com>
2021-05-01 17:53:32 -0700

    Basic Movement Done
    No dash yet

--- gameh/Assets/Scripts/PlayerMovement.cs
+++ gameh/Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,26 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 4f;
     public float gravityMultiplier = 3f;
+    public bool canJump = true;
 
     public Rigidbody2D rb; 
-    
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.collider.tag == "Ground")
+        {
+            canJump = true; 
+        }
+    }
+
     void Update()
     {
         var move = Input.GetAxisRaw("Horizontal");
-         transform.position = transform.position + new Vector3(move * moveSpeed * Time.deltaTime, 0, 0);
+        transform.position = transform.position + new Vector3(move * moveSpeed * Time.deltaTime, 0, 0);
 
-        if (Input.GetButton("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
+        if (Input.GetButton("Jump") && canJump == true) //Mathf.Abs(rb.velocity.y) < 0.001f
         {
+            canJump = false;
             rb.velocity = Vector2.up * jumpForce;
         }
 
@@ -25,6 +35,5 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (gravityMultiplier - 1) * Time.deltaTime;
         }
-
     }
 }
