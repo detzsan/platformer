828ce35a4de9819ebaa18900527ece0da13b99a8
Danyul <scratchmaker500@gmail.com>
2021-05-14 19:38:36 -0700

    PC
    Added canJump and canDash, changed isGrounded. still need to fix the problem of the dash sliding when on the ground.

--- gameh/Assets/Scripts/PlayerMovement.cs
+++ gameh/Assets/Scripts/PlayerMovement.cs
@@ -8,27 +8,29 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 4f;
     public float gravityMultiplier = 3f;
+    public float dashSpeed = 10f;
+    public float dashCooldown = 0.5f;
 
+     public bool canJump;
+     public bool canDash;
      public bool isGrounded;
  
     public Rigidbody2D rb;
 
-    void OnCollisionEnter2D(Collision2D col)
+    void OnCollisionStay2D(Collision2D col)
     {
         if(col.collider.tag == "Ground")
         {
+            canJump = true;
+            canDash = true;
             isGrounded = true;
         }
     }
 
     void OnCollisionExit2D(Collision2D col)
     {
-        if(col.gameObject.tag == "Ground")
-        {
-            isGrounded = false;
-        }
+        isGrounded = false;
     }
-
 
     void FixedUpdate()
     {
@@ -36,9 +38,10 @@ public class PlayerMovement : MonoBehaviour
         transform.position = transform.position + new Vector3(move * moveSpeed * Time.fixedDeltaTime, 0, 0);
         
 
-        if (Input.GetButton("Jump") && isGrounded == true) //Mathf.Abs(rb.velocity.y) < 0.001f
+        if (Input.GetButton("Jump") && canJump == true) //Mathf.Abs(rb.velocity.y) < 0.001f
         {
             rb.velocity = Vector2.up * jumpForce;
+            canJump = false;
         }
 
         if(rb.velocity.y < 0)
@@ -50,10 +53,23 @@ public class PlayerMovement : MonoBehaviour
 
 
         // expand this code, make it so the buttons you press also determine direction. for example, make hitting only dash make u go forward, pressing up make u go up, pressing up and right make u go up and right, etc.
-        if ((Input.GetKey(KeyCode.G)) && isGrounded == true)
+        if ((Input.GetKey(KeyCode.G)) && canDash == true)
         {
-            rb.AddForce(new Vector2(3,0), ForceMode2D.Impulse);
-            
+            canDash = false;
+            rb.AddForce(new Vector2(dashSpeed,0), ForceMode2D.Impulse);
+
+            StartCoroutine("DashTimer");
+        }
+    }
+
+    // Timer to wait for dash to return.
+    IEnumerator DashTimer()
+    {   
+        // If they touched the ground, then isGrounded is set to true and they can dash again.
+        if(isGrounded == true)
+        {
+            yield return new WaitForSeconds(dashCooldown);
+            canDash = true;
         }
     }
 }
