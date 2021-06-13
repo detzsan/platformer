3bfdb8d99c013b190f32f4824a503ba52131ddc1
Danyul <scratchmaker500@gmail.com>
2021-06-13 14:59:53 -0700

    Moving & dissapearing platforms
    disappearing platforms are glitched in unity

---
+++ gameh/Assets/Dissapear.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissapear : MonoBehaviour
+{
+    public bool colliderOn;
+
+    void Start()
+    {
+        colliderOn = true;
+        InvokeRepeating("ColliderCheck", 2f, 2f);
+    }
+
+    void ColliderCheck()
+    {
+            if(colliderOn == true)
+            {
+                this.GetComponent<Collider2D>().enabled = false;
+                colliderOn = false;
+            }
+            
+            if(colliderOn == false)
+            {
+                this.GetComponent<Collider2D>().enabled = true;
+                colliderOn = true;
+            }
+    }
+}
